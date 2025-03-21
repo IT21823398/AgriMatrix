e6f8437ce441dfe7cd937fbd47fbe2bebfce8dab
IT21823398 <shehanofficial2000@gmail.com>
2025-03-21 14:20:22 +0530

    created const Cube with size, xOffset and delay as arguments

--- frontend/src/components/MatrixCubesLogo.jsx
+++ frontend/src/components/MatrixCubesLogo.jsx
@@ -2,4 +2,10 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./MatrixCubesLogo.css"; // Import the CSS file
 
-
+const Cube = ({ size, xOffset, delay }) => {
+    return (
+      
+    );
+  };
+  
+ 
