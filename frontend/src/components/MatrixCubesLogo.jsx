278febf42bb4b6f074af3e9d76ff8e93f56f87b3
IT21823398 <shehanofficial2000@gmail.com>
2025-03-21 14:20:50 +0530

    implemented motion.div initial

--- frontend/src/components/MatrixCubesLogo.jsx
+++ frontend/src/components/MatrixCubesLogo.jsx
@@ -4,7 +4,10 @@ import "./MatrixCubesLogo.css"; // Import the CSS file
 
 const Cube = ({ size, xOffset, delay }) => {
     return (
-      
+      <motion.div
+        initial={{ opacity: 1, y: -120, x: xOffset, scale: 1 }}
+        
+      />
     );
   };
   
