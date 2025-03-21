67a48520ef8c79857006f9d3fd4d0db2cb430651
IT21823398 <shehanofficial2000@gmail.com>
2025-03-21 14:21:07 +0530

    implemented animate

--- frontend/src/components/MatrixCubesLogo.jsx
+++ frontend/src/components/MatrixCubesLogo.jsx
@@ -6,6 +6,12 @@ const Cube = ({ size, xOffset, delay }) => {
     return (
       <motion.div
         initial={{ opacity: 1, y: -120, x: xOffset, scale: 1 }}
+        animate={{
+          opacity: [1, 1, 0], // Keeps full opacity, then fades
+          y: 276, // Move animation slightly up
+          x: xOffset + (Math.random() * 140 - 70), // Shift further right
+          scale: 0.8,
+        }}
         
       />
     );
