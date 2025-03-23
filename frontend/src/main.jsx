3388478f8ee503089607d5585f9286b9a5883d80
IT21823398 <shehanofficial2000@gmail.com>
2025-03-23 11:24:24 +0530

    formtatted main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import './styles/index.css'
 
 import MatrixCubesLogo from './components/MatrixCubesLogo';
 import LandingPage from './pages/LandingPage';
-import './styles/index.css'
+
 
 
 createRoot(document.getElementById('root')).render(
