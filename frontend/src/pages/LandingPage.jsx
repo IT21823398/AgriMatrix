564e4fbfe5e807a0469c831943d0d0f5aab18b48
IT21823398 <shehanofficial2000@gmail.com>
2025-03-23 10:52:46 +0530

    created LandingPage function

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,3 +2,12 @@ import { Link } from 'react-router-dom';
 import './styles/LandingPage.css';
 import google from './assets/google.png';
 import facebook from './assets/facebook.png';
+
+function LandingPage() {
+   
+}
+
+
+
+
+
