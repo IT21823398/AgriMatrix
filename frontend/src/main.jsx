c214c63e5f061887a2a3aa0022c8c0fbb183e483
IT21823398 <shehanofficial2000@gmail.com>
2025-03-23 11:23:54 +0530

    imported App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
 import MatrixCubesLogo from './components/MatrixCubesLogo';
 import LandingPage from './pages/LandingPage';
 import './styles/index.css'
