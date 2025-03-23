3cd191b032d01d08e120e159801bbd09c7379aa1
IT21823398 <shehanofficial2000@gmail.com>
2025-03-23 11:02:57 +0530

    formatted the document

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -7,19 +7,17 @@ function LandingPage() {
     return (
         <body>
             <div class="container">
-            <div class="sign-in">
-            <p id="p1">For Tomorrow's</p>
-            <p id="p2">Future.</p>
-            <button class="s-btn"><img src={google} width="18px" height="18px" />Sign up with Google</button>
-            <br />
+                <div class="sign-in">
+                    <p id="p1">For Tomorrow's</p>
+                    <p id="p2">Future.</p>
+                    <button class="s-btn"><img src={google} width="18px" height="18px" />Sign up with Google</button>
+                    <br />
                     <button class="s-btn"><img src={facebook} width="18px" height="18px" />Sign up with Facebook</button>
                     <hr id="horizontal" />
                     <Link to="/sign-in">
                         <button className="c-btn">Sign in</button>
                     </Link>
-                    
                 </div>
-
             </div>
         </body>
     );
