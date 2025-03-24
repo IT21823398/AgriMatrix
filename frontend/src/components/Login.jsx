85fda26fdf84cc290ea21e68398bd84af4d2ae4f
IT21823398 <shehanofficial2000@gmail.com>
2025-03-24 20:37:00 +0530

    implemented button to request an account

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,26 +4,26 @@ import facebook from '../assets/facebook.png';
 
 function Login() {
     return (
-            <div class="login-container">
-                <div class="sign-in">
-                    <p id="p1">Next Future</p>
-                    <p id="p2">Agriculture.</p>
-                    <form id="sign-in-form">
-                        <input type="text" id="username" name="username" placeholder="Username" required/>
-                        <input type="password" id="password" name="password" placeholder="Password" required />
-                        <br /><br />
-                        <button type="submit" className="submit-btn">Log in</button>
-                        <br /><br />
-                        <hr id="horizontal" />
-                        <br /><br />
-                        <button class="Oauth-btn"><img src={google} height="16px" />Sign in with Google</button>
-                        <br />
-                        <button class="Oauth-btn"><img src={facebook} height="16px" />Sign in with Facebook</button>
-                        <p id="no_account">Not yet registered?</p>
-
-                    </form>
-                </div>
+        <div class="login-container">
+            <div class="sign-in">
+                <p id="p1">Next Future</p>
+                <p id="p2">Agriculture.</p>
+                <form id="sign-in-form">
+                    <input type="text" id="username" name="username" placeholder="Username" required />
+                    <input type="password" id="password" name="password" placeholder="Password" required />
+                    <br /><br />
+                    <button type="submit" className="submit-btn">Log in</button>
+                    <br /><br />
+                    <hr id="horizontal" />
+                    <br /><br />
+                    <button class="Oauth-btn"><img src={google} height="16px" />Sign in with Google</button>
+                    <br />
+                    <button class="Oauth-btn"><img src={facebook} height="16px" />Sign in with Facebook</button>
+                    <p id="no_account">Not yet registered?</p>
+                    <button className="create_ac">Create Account</button>
+                </form>
             </div>
+        </div>
     );
 }
 
