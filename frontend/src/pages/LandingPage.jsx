4edf2144ccb7ab87c3a38929f6e17c1b0aa03cda
IT21823398 <shehanofficial2000@gmail.com>
2025-03-27 19:28:42 +0530

    imported LeafAnim component for the landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import Login from "../components/Login";
 import MatrixCubesLogo from "../components/MatrixCubesLogo";
+import LeafAnim from "../components/LeafAnim";
 
 function LandingPage() {
   return (
