a2f0fbcc7762f55f6a105c486275ec08464783c0
IT21823398 <shehanofficial2000@gmail.com>
2025-03-27 19:26:52 +0530

    imported styles

--- frontend/src/components/LeafAnim.jsx
+++ frontend/src/components/LeafAnim.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import leaf from "../assets/leaf.png";
+import "../styles/MatrixCubesLogo.css";
 
 const LeafAnim = () => {
     return (
